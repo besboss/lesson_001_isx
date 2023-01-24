38fe160bb62abed3f55c1dc8f4c0b449fba16c6b
besboss <giHit@ci01ub>
2023-01-24 13:22:32 +0200

    rename os_info to -> ...v2

--- lesson_001/red_hat/01_os_info.py
+++ lesson_001/red_hat/01_os_info.py
@@ -10,5 +10,5 @@ import sys
 info = 'OS info is \n{}\n\nPython version is {} {}'.format(platform.uname(), sys.version, platform.architecture())
 print(info)
 
-with open('os_info.txt', 'w') as ff:
+with open('os_info_v2.txt', 'w') as ff:
     ff.write(info)
